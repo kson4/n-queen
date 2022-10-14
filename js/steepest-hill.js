b4f257293fe0d02c981ced237bffd6a3b0d8199a
david <davidsinternetmail@gmail.com>
2022-10-14 14:41:56 -0700

    fixed queen conflict calculations

--- js/steepest-hill.js
+++ js/steepest-hill.js
@@ -8,23 +8,24 @@ function getConflicts() {
   board.queens.forEach((k, v) => {
     console.log("looking at queen: ", k, v);
     board.queens.forEach((k2, v2) => {
-      // check columns
-      if (v !== v2 && k == k2 && !(cols.has(k, k2) || cols.has(k2, k))) {
-        console.log("COL: ", k, v, k2, v2);
-        conflicts++;
-        cols.set(k, k2);
-      }
-      // check diagonals
-      if (
-        v !== v2 &&
-        Math.abs(v - v2) == Math.abs(k - k2) &&
-        !(diags.has(v, v2) || diags.has(v2, v))
-      ) {
-        console.log("DIAG", k, v, k2, v2);
-        conflicts++;
-        diags.set(v, v2);
+      if (v2 > v) {
+        if (v !== v2 && k == k2 && !(cols.has(v2, v) || cols.has(v, v2))) {
+          console.log("COL: ", k, v, k2, v2);
+          conflicts++;
+        }
+        if (
+          v !== v2 &&
+          Math.abs(v - v2) === Math.abs(k - k2) &&
+          !(diags.has(v2, v) || diags.has(v, v2))
+        ) {
+          console.log("DIAG", k, v, k2, v2);
+          conflicts++;
+          diags.set(v, v2);
+          console.log(diags);
+        }
       }
     });
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
   });
   console.log(conflicts);
 }
